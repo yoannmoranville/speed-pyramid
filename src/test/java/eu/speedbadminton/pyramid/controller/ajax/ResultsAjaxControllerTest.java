19d79c8b8580caa4c495536aca2bdc084e14d5d7
lukesolar <l.gotter@gmail.com>
2013-12-05 15:15:09 +0100

    Test for match date check between creation and today

--- src/test/java/eu/speedbadminton/pyramid/controller/ajax/ResultsAjaxControllerTest.java
+++ src/test/java/eu/speedbadminton/pyramid/controller/ajax/ResultsAjaxControllerTest.java
@@ -24,6 +24,9 @@ public class ResultsAjaxControllerTest {
 
         Calendar matchDate = Calendar.getInstance();
         //matchDate.add(Calendar.DAY_OF_WEEK,-1); // yesterday
+        //Date match = new Date(Date.parse("2013-11-30T12:08:40.889Z")); // thats how it comes from DB
+        //Date creation = new Date(Date.parse("2013-11-29T23:00:00Z"));
+        //Assert.assertTrue("parsed from DB workd",ResultsAjaxController.isDateCorrect(creation,match));
 
         // matchdate before creation
         matchDate.set(2013,Calendar.JANUARY,14, 16, 8);
@@ -41,9 +44,10 @@ public class ResultsAjaxControllerTest {
         matchDate.setTime(new Date());
         Assert.assertTrue("matchdate today",ResultsAjaxController.isDateCorrect(creationDate.getTime(), matchDate.getTime()));
 
-        // matchDate tomorrow (fails)
+        // matchDate tomorrow (should fail)
         matchDate.add(Calendar.DAY_OF_WEEK,1);
-        Assert.assertFalse("matchdate today",ResultsAjaxController.isDateCorrect(creationDate.getTime(), matchDate.getTime()));
+        printDates(creationDate,matchDate);
+        Assert.assertFalse("matchdate tomorrow",ResultsAjaxController.isDateCorrect(creationDate.getTime(), matchDate.getTime()));
 
     }
 
