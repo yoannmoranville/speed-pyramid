72766d45dd9d698133dc71de3326289efdb2c410
yoannmoranville <yoann.moranville@gmail.com>
2013-12-14 14:53:09 +0100

    clean

--- src/test/java/eu/speedbadminton/pyramid/controller/ajax/ResultsAjaxControllerTest.java
+++ src/test/java/eu/speedbadminton/pyramid/controller/ajax/ResultsAjaxControllerTest.java
@@ -20,13 +20,7 @@ public class ResultsAjaxControllerTest {
         Calendar creationDate = Calendar.getInstance();
         creationDate.set(2013, Calendar.JANUARY, 15, 12, 56);
 
-        Calendar today = Calendar.getInstance();
-
         Calendar matchDate = Calendar.getInstance();
-        //matchDate.add(Calendar.DAY_OF_WEEK,-1); // yesterday
-        //Date match = new Date(Date.parse("2013-11-30T12:08:40.889Z")); // thats how it comes from DB
-        //Date creation = new Date(Date.parse("2013-11-29T23:00:00Z"));
-        //Assert.assertTrue("parsed from DB workd",ResultsAjaxController.isDateCorrect(creation,match));
 
         // matchdate before creation
         matchDate.set(2013,Calendar.JANUARY,14, 16, 8);
@@ -42,17 +36,10 @@ public class ResultsAjaxControllerTest {
 
         // matchDate today
         matchDate.setTime(new Date());
-        printDates(creationDate,matchDate); //todo: If I comment this line, the test fails, WTH?!
         Assert.assertTrue("matchdate today",ResultsAjaxController.isDateCorrect(creationDate.getTime(), matchDate.getTime()));
 
         // matchDate tomorrow (should fail)
         matchDate.add(Calendar.DAY_OF_MONTH,1);
-        printDates(creationDate,matchDate);
         Assert.assertFalse("matchdate tomorrow",ResultsAjaxController.isDateCorrect(creationDate.getTime(), matchDate.getTime()));
-
-    }
-
-    private static void printDates(Calendar created, Calendar match){
-        System.out.println("created:"+created.getTime()+" - match:"+match.getTime());
     }
 }
